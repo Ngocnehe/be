4b4976eadc584853a4d8c16899c49080d36704c9
NgoC <154618130+Ngocnehe@users.noreply.github.com>
2025-05-19 09:32:39 +0700

    Update cloudinary.config.ts

--- src/cloudinary/cloudinary.config.ts
+++ src/cloudinary/cloudinary.config.ts
@@ -10,9 +10,9 @@ export const CloudinaryConfig = {
   provide: 'Cloudinary',
   useFactory: () => {
     return cloudinary.config({
-      cloud_name: 'dmsuwoosx',
-      api_key: '819541955439382',
-      api_secret: 'cahdIEgZoAw4zzojlokJ4GlTgz0',
+      cloud_name: 'dl13c9cbq',
+      api_key: '961994793696782',
+      api_secret: 'AwbocsmWL4YEGURC03FjL1Pdfu4',
     });
   },
 };
